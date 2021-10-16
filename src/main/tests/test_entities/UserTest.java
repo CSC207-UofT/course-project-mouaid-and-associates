a3f489cdd4b6a6b48c718e20cb03cc8640a500bd
Sujoy Deb Nath <sujoy.debnath@mail.utoronto.ca>
2021-10-16 02:26:38 -0400

    Moauid completed the UserTest. Mohamed completed UserManagerTest.

---
+++ src/main/tests/test_entities/UserTest.java
@@ -0,0 +1,19 @@
+package test_entities;
+
+import org.junit.jupiter.api.Test;
+import entities.User;
+import entities.Medicine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+
+    @Test
+    public void TestAddMedicine(){
+        User myUser = new User("Mouaid Alim", "alimmoua");
+        Medicine myMedicine = new Medicine("Mouaid's medicine", 60, "Injection",
+                              "needs to be taken 1 hour before eating");
+        myUser.addMedicine(myMedicine);
+        assertTrue(myUser.getMedicineList().containsValue(myMedicine));
+    }
+}
