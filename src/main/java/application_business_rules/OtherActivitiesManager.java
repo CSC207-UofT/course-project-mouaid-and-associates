73769bfd2ad2f8caed184f56ef22535ea8a0f8d1
Mouaid-Alim <mouaidalim1@gmail.com>
2021-11-09 17:11:03 -0500

    Changed SleepManager and SleepSchedule into OtherActivitiesManager and OtherActivitiesSchedule respectively, so that now instead of just being for the Sleep class, they work for all activity classes like Sleep and Meal using polymorphism.

---
+++ src/main/java/application_business_rules/OtherActivitiesManager.java
@@ -0,0 +1,49 @@
+package application_business_rules;
+
+import entities.Meal;
+import entities.OtherActivities;
+import entities.Schedule;
+import entities.Sleep;
+
+import java.util.List;
+
+public class OtherActivitiesManager {
+    /**
+     * This class manages the SleepSchedule and the MealSchedule
+     */
+    public OtherActivitiesManager(){}
+
+    /**
+     * Create a default Sleep Class with an empty SleepSchedule
+     * @return The created Sleep class
+     */
+    public Sleep createNewSleepClass(){
+        Sleep sleepClass = new Sleep();
+        sleepClass.createSchedule();
+        return sleepClass;
+    }
+
+    public Meal createNewMealClass(){
+        Meal mealClass = new Meal();
+        mealClass.createSchedule();
+        return mealClass;
+    }
+
+    /**
+     * Get the ActivitySchedule of the given OtherActivities Class
+     * @param activity The OtherActivities class to get the Schedule from
+     * @return This OtherActivities Class' Schedule
+     */
+    public Schedule getActivitySchedule(OtherActivities activity) {
+        return activity.getMySchedule();
+    }
+
+    /**
+     * Set the times of the given OtherActivities Class
+     * @param activity The OtherActivities class to set the times for
+     * @param times A list containing the new times
+     */
+    public void setActivityTimes(OtherActivities activity, List<Double> times){
+        activity.setTime(times);
+    }
+}
