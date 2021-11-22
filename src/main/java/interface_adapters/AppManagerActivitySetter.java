90ab9fc4344c091e9537a231bbbe0d14fde3b906
Mouaid-Alim <mouaidalim1@gmail.com>
2021-11-22 11:32:22 -0500

    Finished the AppManagerActivitySetter, AppManagerMedicine and AppManagerPrescription Classes inorder to create our AppManagerFacade

--- src/main/java/interface_adapters/AppManagerActivitySetter.java
+++ src/main/java/interface_adapters/AppManagerActivitySetter.java
@@ -1,4 +1,65 @@
 package interface_adapters;
 
+import application_business_rules.ManagementSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class AppManagerActivitySetter {
+    /**
+     * The class that takes care of setting the activity times in the AppManagerFacade
+     */
+
+    Map<String, Window> windows;
+    ManagementSystem managementSystem;
+
+    public AppManagerActivitySetter(Map<String, Window> windows, ManagementSystem managementSystem) {
+        this.windows = windows;
+        this.managementSystem = managementSystem;
+    }
+
+    /**
+     * Shows the set sleep times window, and allows the user to set their sleep schedule.
+     * @return  The name of the next window to open.
+     */
+    public String setSleepTimes(){
+        // Call SetSleepTimingsWindow to display the fields to enter the sleep and wakeup times
+        Window setSleepTimingsWindow = windows.get("Set Sleep Timings Window");
+
+        String[] stringTimings = setSleepTimingsWindow.getUserInput();
+
+        String sleepTime = stringTimings[0];
+        String wakeUpTime = stringTimings[1];
+
+        List<String> times = new ArrayList<>();
+        times.add(sleepTime);
+        times.add(wakeUpTime);
+
+        this.managementSystem.setSleepAndWakeUpTimes(times);
+
+        // Return to the account page.
+        return "View Account Window";
+    }
+
+    /**
+     * Shows the set meal times window, and allows the user to set their meal schedule.
+     * @return  The name of the next window to open.
+     */
+    public String setMealTimes(){
+        // Call SetMealTimingsWindow to display the fields to enter the Meal times
+        Window setMealTimingsWindow = windows.get("Set Meal Timings Window");
+
+        String[] stringTimings = setMealTimingsWindow.getUserInput();
+        List<String> times = new ArrayList<>();
+
+        for (String timings: stringTimings){
+            times.add(timings);
+        }
+
+        this.managementSystem.setMealTimes(times);
+
+        // Return to the account page.
+        return "View Account Window";
+    }
 }
--- src/main/java/interface_adapters/AppManagerMedicine.java
+++ src/main/java/interface_adapters/AppManagerMedicine.java
@@ -1,4 +1,121 @@
 package interface_adapters;
 
+import application_business_rules.ManagementSystem;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class AppManagerMedicine {
+    /**
+     * The class that takes care of Medicine related tasks in the AppManagerFacade
+     */
+
+    Map<String, Window> windows;
+    ManagementSystem managementSystem;
+    AppManagerHelpers appManagerHelpers;
+
+    public AppManagerMedicine(Map<String, Window> windows, ManagementSystem managementSystem) {
+        this.windows = windows;
+        this.managementSystem = managementSystem;
+        this.appManagerHelpers = new AppManagerHelpers(managementSystem);
+    }
+
+    /**
+     * Allows the user to pick a specific medicine and edit that medication's information and the times
+     * to take that medication.
+     */
+    public String editMedicine(){
+        // First instantiate a window for EditMedicineWindow
+        Window editMedicineWindow = windows.get("Edit Medicine Window");
+        // Next, instantiate a window for ChooseMedicineToEditWindow
+        Window chooseMedicineToEditWindow = windows.get("Choose Medicine To Edit Window");
+
+        // Get a list of medicine names, like for removeMedicine
+        // First get a list of user information
+        String[] bigList = managementSystem.getUserInfo().toArray(new String[0]);
+
+        // Isolate and get the list of medicine names
+        String[] medList = this.appManagerHelpers.getFormattedList("List of medicines to choose from: ", bigList,
+                2, bigList.length);
+        // First, instantiate a variable to store the name of the medicine the user would like to remove.
+        String medName;
+        String[] medInfo;
+        String[] changes; // Represents the changes the user wants to make.
+        List<LocalDateTime> newTimes = new ArrayList<>();
+
+        // Check if ChooseMedicineToEditWindow is an instance of DisplayEntityInformation to print
+        // out the list of medicine names, like in removeMedicine()
+        if (chooseMedicineToEditWindow instanceof DisplayEntityInformation){
+            ((DisplayEntityInformation) chooseMedicineToEditWindow).displayInfo(medList);
+        }
+
+
+        medName = chooseMedicineToEditWindow.getUserInput()[0];
+
+        medInfo = managementSystem.getMedicineInfo(medName);
+
+        if (editMedicineWindow instanceof DisplayEntityInformation){
+            ((DisplayEntityInformation) editMedicineWindow).displayInfo(medInfo);
+        }
+
+        changes = editMedicineWindow.getUserInput();
+
+        // Format the given times.
+        if (changes.length > 5) {
+            appManagerHelpers.formatTimes(changes, changes[5], changes[6], changes[7], newTimes);
+        }
+
+        // Call management system to edit the entities.
+        managementSystem.editMedicine(medName, changes, newTimes);
+
+        return "View Account Window";
+    }
+
+
+
+    /** This method is to run the functionality of removing a medicine, from showing the menu to
+     * calling the appropriate classes to remove the medicine from the account.
+     */
+    public String removeMedicine(){
+        Window removeMedWindow = windows.get("Remove Medicine Window");
+        String[] userInfo = managementSystem.getUserInfo().toArray(new String[0]);
+
+        // Get a formatted list to make the printing of the medicine names nice.
+        String[] infoToPrint = appManagerHelpers.getFormattedList("The list of medicines to pick from: ",
+                userInfo, 2, userInfo.length);
+
+        // Display the list of medicine names.
+        if (removeMedWindow instanceof DisplayEntityInformation){
+            ((DisplayEntityInformation) removeMedWindow).displayInfo(infoToPrint);
+        }
+
+        // Get the list of medicine names the user wants to remove
+        String[] inputs = removeMedWindow.getUserInput();
+
+        // Call management system to remove the medicines
+        managementSystem.removeMedicines(inputs);
+
+        // go back to the account page.
+        return "View Account Window";
+    }
+
+    /**
+     * Shows the add medicine page. Allows user interaction with the program so that
+     * the user can add a new medicine.
+     */
+    public String addMedicine(){
+        //Done: call AddMedicineWindow to display the fields to enter data about the medicine
+        Window addMedicineWindow = windows.get("Add Medicine Window");
+
+        String[] data = addMedicineWindow.getUserInput();
+
+        appManagerHelpers.addMedicineHelper(data);
+
+        //Done: call showAccountWindow
+        return "View Account Window";
+    }
+
+
 }
