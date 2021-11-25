5e56cd776fda484ef7d5ae962b91b03ce3e68a67
Mouaid-Alim <mouaidalim1@gmail.com>
2021-11-25 16:20:23 -0500

    Created the ManagementSystemPrescription class which is the class that takes care of all prescription related tasks in the ManagementSystemFacade

---
+++ src/main/java/application_business_rules/ManagementSystemPrescription.java
@@ -0,0 +1,11 @@
+package application_business_rules;
+
+import entities.Medicine;
+import entities.PrescriptionMedicine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManagementSystemPrescription {
+
+}
