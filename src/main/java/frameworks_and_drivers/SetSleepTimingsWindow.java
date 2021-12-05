22c3e03a392eeb021d758a5d4aed67dc874e0ecd
Mouaid-Alim <mouaidalim1@gmail.com>
2021-12-04 19:08:17 -0500

    Committed so I can pull from main

--- src/main/java/frameworks_and_drivers/SetSleepTimingsWindow.java
+++ src/main/java/frameworks_and_drivers/SetSleepTimingsWindow.java
@@ -15,8 +15,9 @@ public class SetSleepTimingsWindow extends Window {
      */
 
     private List<JTextField> changes;
+    private List<JLabel> changeLabels;
     private JPanel panel;
-    private List<String> userInput;
+    private volatile List<String> userInput;
     private Font labelFont;
     private List<JLabel> infoOfTimes;
 
@@ -54,14 +55,6 @@ public class SetSleepTimingsWindow extends Window {
         // Return user input.
         return userResponse;
 
-
-
-
-
-
-
-
-
 //        String[] returnList = new String[2];
 //        System.out.println("Please enter your sleep time?");
 //        String sleepTime = scanner.nextLine();
@@ -90,6 +83,70 @@ public class SetSleepTimingsWindow extends Window {
         panel.setLayout(null);
         super.setPanelSize(panel);
 
+        // Create a list of messages to print onto the labels:
+        String[] labels = new String[]{"Please enter your sleep time?", "Please enter your wake up time?"};
+
+        // Create all the text boxes but don't set their locations yet.
+        changes = new ArrayList<>();
+        for (int i = 0; i < 2; i++){
+            JTextField textField = new JTextField("");
+            textField.setSize(286, 60);
+            changes.add(textField);
+            panel.add(textField);
+        }
+
+        // Create all the corresponding labels but don't set their locations yet.
+        changeLabels = new ArrayList<>();
+        for (String label: labels){
+            JLabel changeLabel = new JLabel(label);
+            changeLabel.setSize(466, 60);
+            changeLabel.setFont(labelFont);
+
+            changeLabels.add(changeLabel);
+            panel.add(changeLabel);
+        }
+
+        // Create the button.
+        Font buttonFont = new Font("SansSerif", Font.BOLD, 15);
+        JButton saveButton = new JButton("SAVE AND RETURN TO ACCOUNT PAGE");
+        saveButton.setFont(buttonFont);
+        saveButton.setSize(400, 70);
+
+        panel.add(saveButton);
+
+        // Add the button to the button map.
+        super.buttonResponses.put(saveButton, "0");
+
+        // Add an action listener for each button.
+        super.addActionListenerToAllButtons();
+
+
+    }
+
+    /**
+     * Sets the locations for all the attributes of this class, specifically
+     * changes, changeLabels, and saveButton. Since we don't know how much information
+     * will need to be printed for the medicine, we need the location of the lowest
+     * label on the panel to build of the rest.
+     *
+     * @param y     The y coordinate of the last label.
+     */
+    private void setLocations(int y){
+        // Ensures there are the same number of labels as there are text boxes.
+        assert (changes.size() == changeLabels.size());
+
+        for (int i = 0; i < changes.size(); i++){
+            changeLabels.get(i).setLocation(20, y + 20 + 140 * i);
+            changes.get(i).setLocation(20, y + 80 + 140 * i);
+        }
+
+        for (JButton button: super.buttonResponses.keySet()){
+            button.setLocation(43, y + 40 + 140 * changes.size());
+        }
+
+        panel.setPreferredSize(new Dimension(486, y + 160 + 140 * changes.size()));
+
+        super.view.revalidate();
     }
 
     /**
