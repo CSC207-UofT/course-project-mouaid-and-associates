e54bd23e98c7154d1a8182c9bbbcc5637d4021b8
Mouaid-Alim <mouaidalim1@gmail.com>
2021-12-05 22:12:51 -0500

    Finished the SetMealTimingsWindow GUI and reflected the change in main

--- src/main/java/frameworks_and_drivers/SetMealTimingsWindow.java
+++ src/main/java/frameworks_and_drivers/SetMealTimingsWindow.java
@@ -3,7 +3,10 @@ package frameworks_and_drivers;
 import interface_adapters.ObservableFrame;
 import interface_adapters.Window;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,40 +15,129 @@ public class SetMealTimingsWindow extends Window {
      * The window that displays the Set Meal Time page.
      */
 
-    public SetMealTimingsWindow(Scanner scanner, ObservableFrame frame){ super(scanner, frame);}
+    private JTextField mealNumber;
+    private SelectTimesWindow selectTimes;
+    private String[] times;
+    private JPanel panel;
+    private volatile List<String> userInput;
+
+
+    public SetMealTimingsWindow(Scanner scanner, ObservableFrame frame, SelectTimesWindow selectTimes){
+        super(scanner, frame);
+        this.selectTimes = selectTimes;
+        mealNumber = new JTextField();
+        createView();
+        userInput = new ArrayList<>();
+
+    }
 
     /**
-     * Gets input from the user in order to Set the Meal Times.
+     * Gets input from the user in order to Set the Sleeping Time.
      * @return A list containing the user login input
      */
     @Override
     public String[] getUserInput() {
-        List<String> returnList = new ArrayList<>();
-        System.out.println("How many meals do you have in a day?");
-        String numberOfMeals = scanner.nextLine();
-        int numbOfMeals = Integer.parseInt(numberOfMeals);
-        for (int i = 0; i < numbOfMeals; i++){
-            int u = i + 1;
-            System.out.println("Please enter the time for meal number: " + u);
-            String mealTime = scanner.nextLine();
-            returnList.add(mealTime);
+        String[] userResponse = new String[0];
+        boolean askedTimes = false;
+
+        while (!(super.userResponded && userResponse.length >= 1)){
+
+            if (super.userResponded && !askedTimes){
+//                selectTimes.setNumTimes(Integer.parseInt(changes.get(0).getText()));
+                selectTimes.setNumTimes(Integer.parseInt(mealNumber.getText()));
+                selectTimes.updateFrame();
+                times = selectTimes.getUserInput();
+                askedTimes = true;
+            }
+            // Only when we have a valid number of inputs do
+            // we get all the user input.
+            if (userInput.size() == 1 && askedTimes) {
+                userResponse = times;
+                System.out.println(Arrays.toString(userResponse));
+            }
+
         }
 
-        return returnList.toArray(new String[0]);
+        // Clear the input list so that we can use it again next time.
+        userInput.clear();
+
+        // Empty the text box
+        mealNumber.setText("");
+
+        // Return user input.
+        return userResponse;
     }
 
     /**
-     * Notify the observer of a change
-     *
-     * @param source
+     * Sets userInput if the source is the save button in this view.
+     * This method is synchronized so that when multiple threads
+     * access this variable, we can ensure that only one thread
+     * can access this method at a time.
+     * @param source   The source of the event.
      */
     @Override
     public void update(Object source) {
+        if (super.buttonResponses.containsKey((JButton) source)){
+            // Since changes is an ordered list of text boxes, thus
+            // we just add the items to the userInput.
 
+            userInput.add(mealNumber.getText());
+//            for (int i = 0; i < changes.size(); i++) {
+//                userInput.add(changes.get(i).getText());
+//            }
+
+            // Best to do this after you make all your changes.
+            super.userResponded = true;
+        }
     }
 
     @Override
     public void createView() {
+        // Create a new JPanel
+        panel = new JPanel();
+        panel.setLayout(null);
+        super.setPanelSize(panel);
+
+        // Instantiate the text box
+        JLabel mealLabel = new JLabel("Please enter the number of Meals: ");
+        mealLabel.setSize(286, 40);
+        mealLabel.setLocation(100, 50);
+        mealNumber.setSize(286, 50);
+        mealNumber.setLocation(100, 100);
+
+
+        // Create the button.
+        Font buttonFont = new Font("SansSerif", Font.BOLD, 15);
+        JButton saveButton = new JButton("NEXT");
+        saveButton.setFont(buttonFont);
+        saveButton.setSize(400, 70);
+        saveButton.setLocation(43, 400);
+
+//        changes = new ArrayList<>();
+//        changes.add(numberOfMeals);
+
+        // Add the items to the panel
+        panel.add(mealLabel);
+        panel.add(mealNumber);
+        panel.add(saveButton);
+
+        // Add the button to the button map.
+        super.buttonResponses.put(saveButton, "0");
+
+        // Add an action listener for each button.
+        super.addActionListenerToAllButtons();
+
+        // Set the view for this window.
+        super.view = panel;
 
     }
+
+//    /**
+//     * Resets all the text fields to contain an empty string.
+//     */
+//    private void resetTextFields(){
+//        for (JTextField textField: changes){
+//            textField.setText("");
+//        }
+//    }
 }
