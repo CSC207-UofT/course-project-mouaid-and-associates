c1b8903d9d4d871df887c0f94d1473ec1bdfbeae
Sujoy Deb Nath <sujoy.debnath@mail.utoronto.ca>
2021-12-05 23:01:05 -0500

    Added the functionality of changing the times of a medicine.

--- src/main/java/frameworks_and_drivers/AddPrescriptionWindow.java
+++ src/main/java/frameworks_and_drivers/AddPrescriptionWindow.java
@@ -4,6 +4,7 @@ import interface_adapters.ObservableFrame;
 import interface_adapters.PrescriptionWindow;
 import interface_adapters.Window;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
